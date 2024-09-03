7e158dadd0f18bd151bfd004570557ba0da0e45e
yangjl <2139688756@qq.com>
2024-09-03 22:56:06 +0800

    登录认证功能，返回JWT令牌；添加全局异常处理器

--- src/main/java/com/yangjl/bigevent/controller/UserController.java
+++ src/main/java/com/yangjl/bigevent/controller/UserController.java
@@ -3,26 +3,69 @@ package com.yangjl.bigevent.controller;
 import com.yangjl.bigevent.entity.Result;
 import com.yangjl.bigevent.entity.User;
 import com.yangjl.bigevent.service.UserService;
+import com.yangjl.bigevent.utils.JwtUtil;
+import com.yangjl.bigevent.utils.Md5Util;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    /**
+     * 用户注册接口
+     * @param username 5~16位非空字符组成的用户名
+     * @param password 5~16位非空字符组成的密码
+     * @return
+     */
     @PostMapping("/register")
-    public Result resigter(String username, String password) {
+    public Result resigter(@Pattern(regexp = "^\\S{2,16}$") String username,
+                           @Pattern(regexp = "^\\S{5,16}$") String password) {
         User user = userService.findByUserName(username);
         if (user != null) {
             return Result.error("用户名已被占用");
         }
         userService.register(username, password);
         return Result.success();
+    }
+
+    /**
+     * 登录接口
+     * @param username
+     * @param password
+     * @return
+     */
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{2,16}$") String username,
+                        @Pattern(regexp = "^\\S{5,16}$") String password) {
+        User user = userService.findByUserName(username);
+        if (user == null) {
+            return Result.error("用户不存在");
+        }
+
+        String md5String = Md5Util.getMD5String(password);
+        if(!user.getPassword().equals(md5String)) {
+            return Result.error("密码错误");
+        }
+
+        Map<String, Object> claims = new HashMap<>(){{
+            put("id", user.getId());
+            put("name", user.getUsername());
+        }};
+        String token = JwtUtil.genToken(claims);
+        return Result.success(token);
+
     }
 
 }
