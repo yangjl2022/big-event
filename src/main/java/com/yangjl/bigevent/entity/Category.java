101dfcda850df7456f6bdf41b1293ef903f6e134
yangjl <2139688756@qq.com>
2024-09-04 23:25:54 +0800

    文章分类列表的结果中时间戳的格式修复

--- src/main/java/com/yangjl/bigevent/entity/Category.java
+++ src/main/java/com/yangjl/bigevent/entity/Category.java
@@ -1,9 +1,12 @@
 package com.yangjl.bigevent.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Data
@@ -16,6 +19,9 @@ public class Category {
 
     @JsonIgnore
     private Integer createUserId;
-    private LocalTime createTime;
-    private LocalTime updateTime;
+
+    @JsonFormat(pattern = "yy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+    @JsonFormat(pattern = "yy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
 }
