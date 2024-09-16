25c872efcd4a875dcf1ca03256f66d43972ddd1b
yangjl <2139688756@qq.com>
2024-09-16 09:11:40 +0800

    解决文件URL访问权限问题

---
+++ src/main/java/com/yangjl/bigevent/client/AliOssClient.java
@@ -0,0 +1,91 @@
+package com.yangjl.bigevent.client;
+
+import com.aliyun.oss.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+
+@Slf4j
+@Service
+public class AliOssClient {
+    @Value("${oss.endpoint}")
+    private String endpoint;
+
+    @Value("${oss.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${oss.access-key-secret}")
+    private String accessKeySecret;
+
+    @Value("${oss.bucket-name}")
+    private String bucketName;
+
+    private OSS oss;
+
+    @PostConstruct
+    public void init() {
+        oss = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        if (oss != null) {
+            oss.shutdown();
+        }
+    }
+
+
+    /**
+     * 上传文件,返回文件的公网访问地址
+     * @param objectName
+     * @param inputStream
+     * @return
+     */
+    public String uploadFile(String objectName, InputStream inputStream){
+        //公文访问地址
+        String url = "";
+        try {
+            // 创建存储空间。
+            oss.createBucket(bucketName);
+            oss.putObject(bucketName, objectName, inputStream);
+            url = generatePresignedUrl(objectName, 12);
+            log.info("文件 {} 上传成功：{}", objectName, url);
+        } catch (OSSException oe) {
+            log.info("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            log.info("Error Message: {}", oe.getErrorMessage());
+            log.info("Error Code: {}" , oe.getErrorCode());
+            log.info("Request ID: {}", oe.getRequestId());
+            log.info("Host ID: {}", oe.getHostId());
+        } catch (ClientException ce) {
+            log.info("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            log.info("Error Message: {}", ce.getMessage());
+        }
+        return url;
+    }
+
+    /**
+     * 产生带了访问文件权限的URL
+     * @param objectName
+     * @param expirationInHours
+     * @return
+     */
+    public String generatePresignedUrl(String objectName, int expirationInHours) {
+        // 设置 URL 过期时间
+        Date expiration = new Date(new Date().getTime() + 1000L *60*60* expirationInHours);
+        // 生成公开 URL
+        URL url = oss.generatePresignedUrl(bucketName, objectName, expiration);
+        return url.toString();
+    }
+}
--- src/main/java/com/yangjl/bigevent/controller/FileUploadController.java
+++ src/main/java/com/yangjl/bigevent/controller/FileUploadController.java
@@ -1,20 +1,22 @@
 package com.yangjl.bigevent.controller;
 
 import com.yangjl.bigevent.entity.Result;
-import com.yangjl.bigevent.utils.AliOssUtil;
-import org.springframework.beans.factory.annotation.Value;
+import com.yangjl.bigevent.client.AliOssClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/upload")
 public class FileUploadController {
+    @Autowired
+    AliOssClient aliOssClient;
+
     @PostMapping
     public Result upload(MultipartFile file) throws IOException {
         String originalFilename = file.getOriginalFilename();
@@ -22,7 +24,7 @@ public class FileUploadController {
         String extension = originalFilename.substring(index);
         String stem = originalFilename.substring(0, index);
         String filename = stem + '-' + UUID.randomUUID().toString().substring(0, 8) + extension;
-        String url = AliOssUtil.uploadFile(filename, file.getInputStream());
-        return Result.success("文件成功上传到"+ url);
+        String url = aliOssClient.uploadFile(filename, file.getInputStream());
+        return Result.success(url);
     }
 }
